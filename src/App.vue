bc193a45630e872058fb8b8564d7d036f88a662b
Your Name <you@example.com>
2020-07-30 00:44:27 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Cadeau de départ: démultiplicateur de lignes excel"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,211 @@
 <template>
-  <div class="hello">
+  <div class="YOLO">
     <h1>{{ msg }}</h1>
+
+    <div
+      style="padding: 20px; margin: auto; border-radius: 3px; border-style: solid; border-color: black; width: 75%; text-align: left"
+    >
+      <ul>
+        <li>Les entêtes doivent être sur la première ligne.</li>
+        <li>Le même séparateur est utilisé pour toute les colonnes. Si besoin de séparer selon "," sur une colonne A, et selon " " pour une colonne B, il faut lancer le script deux fois.</li>
+        <li>Attention à ne pas laisser trainer des espaces.</li>
+        <li>
+          Toute la ligne est dupliquée, y compris les cases avec des chiffres,
+          <span style="color:red">attention au totaux</span> !
+        </li>
+        <li>
+          Tout les calculs se font dans le navigateur, il n'y a aucun échange sur le réseau. <span style="color:orange">Si le fichier est gros, c'est votre ordinateur qui ralentit</span> :) <span style="color:green">Mais niveau confidentialités des données c'est safe.</span>
+        </li>
+      </ul>
+    </div>
+
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <input id="file" accept=".xls, .xlsx" type="file" @change="changeFile" />
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <div v-if="sheets_name !== null">
+      <label for="select_sheet" style="margin:10px">Sélectionner le tab</label>
+      <select id="select_sheet" v-model="selected_sheet_name">
+        <option disabled value>Please select one</option>
+        <option v-for="sheet_name in sheets_name" :key="sheet_name">{{ sheet_name }}</option>
+      </select>
+    </div>
+
+    <div style="color: red; margin:10px">{{ error_headers }}</div>
+    <div style="color: red; margin:10px">{{ error_sep }}</div>
+    <div style="color: red; margin:10px">{{ error_col }}</div>
+
+    <div v-if="sheet !== null">
+      <span>Cocher les colonnes à démultiplier:</span>
+      <span v-for="col in headers" :key="col">
+        <input
+          type="checkbox"
+          :id="col"
+          :value="col"
+          v-model="col_to_duplicate"
+          style="margin:10px"
+        />
+        <label :for="col">{{ col }}</label>
+      </span>
+    </div>
+
+    <div>
+      <textarea style="white-space: pre-line" id="sep" v-model="sep" placeholder="Séparateur" />
+    </div>
+
+    <button v-if="this.sheet !== null" @click="go">Expand</button>
+
+    <div v-if="content2 !== ''">
+      <button @click="download">Download</button>
+    </div>
   </div>
 </template>
 
 <script>
+import XLSX from "xlsx";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  data: function () {
+    return {
+      content: "",
+      content2: "",
+      excel_file: null,
+      selected_sheet_name: null,
+      col_to_duplicate: [],
+      error_headers: "",
+      error_sep: "",
+      error_col: "",
+      sep: "",
+    };
+  },
+
+  computed: {
+    sheets_name: {
+      get: function () {
+        if (this.excel_file === null) return null;
+        return this.excel_file.SheetNames;
+      },
+    },
+    sheet: {
+      get: function () {
+        if (this.selected_sheet_name === null) return null;
+        return this.excel_file.Sheets[this.selected_sheet_name];
+      },
+    },
+
+    headers: {
+      get: function () {
+        return this.get_headers();
+      },
+    },
+  },
+
+  methods: {
+    get_headers: function () {
+      if (this.sheet === null) return null;
+      var headers = [];
+      var range = XLSX.utils.decode_range(this.sheet["!ref"]);
+      var C,
+        R = range.s.r; /* start in the first row */
+      /* walk every column in the range */
+      for (C = range.s.c; C <= range.e.c; ++C) {
+        var cell = this.sheet[
+          XLSX.utils.encode_cell({ c: C, r: R })
+        ]; /* find the cell in the first row */
+
+        var hdr = "UNKNOWN " + C; // <-- replace with your desired default
+        if (cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+
+        headers.push(hdr);
+      }
+      if (headers.length != new Set(headers).size) {
+        this.error_headers =
+          "There are columns with the same name resulting in conflict, please change this";
+      } else {
+        this.error_headers = "";
+      }
+      return headers;
+    },
+
+    changeFile: function (e) {
+      (this.excel_file = null), (this.selected_sheet_name = null);
+      this.col_to_duplicate = [];
+      this.content = "";
+      this.content2 = "";
+
+      var f = e.target.files[0];
+      var reader = new FileReader();
+      reader.onload = (e) => {
+        var data = new Uint8Array(e.target.result);
+        var workbook = XLSX.read(data, { type: "array" });
+        this.excel_file = workbook;
+        this.selected_sheet_name = this.excel_file.SheetNames[0];
+      };
+      reader.readAsArrayBuffer(f);
+    },
+
+    go: function () {
+      this.content2 = "";
+      if (this.col_to_duplicate.length < 1) {
+        this.error_col = "Veuillez indiquer au moins une colonne à dupliquer";
+      } else this.error_col = "";
+      if (this.sep === null || this.sep === "") {
+        this.error_sep =
+          'Veuillez indiquer un séparateur (par exemple: ",", " ", <enter>...)';
+      } else this.error_sep = "";
+
+      if (this.error_headers || this.error_sep || this.error_col) return;
+
+      var data = XLSX.utils.sheet_to_json(this.sheet, { header: 1 });
+      this.content = XLSX.utils.sheet_to_json(this.sheet, { header: 1 }); // just a save
+
+      for (const col of this.col_to_duplicate) {
+        // for every col_to_duplicate, we iter over the dataset
+        var result = [];
+        const index = this.headers.findIndex((e) => e == col);
+
+        data.map((row) => {
+          // for each line
+          if (
+            row[index] == null ||
+            row[index].toString().split(this.sep).length <= 1
+          ) {
+            result.push(row.slice()); // to duplicate !
+          } else {
+            for (const dup of row[index].toString().split(this.sep)) {
+              if (dup.trim().length > 0) {
+                result.push(row.slice()); // to duplicate
+                result[result.length - 1][index] = dup;
+              }
+            }
+          }
+        }); // end iter over row
+        data = result;
+        result = [];
+      } //end col_to_duplicate
+
+      this.content2 = data;
+    },
+
+    download: function () {
+      var wb = XLSX.utils.book_new();
+      var ws = XLSX.utils.aoa_to_sheet(this.content2);
+      XLSX.utils.book_append_sheet(
+        wb,
+        ws,
+        this.selected_sheet_name + "_deduplicated"
+      );
+      XLSX.writeFile(wb, "output.xlsx");
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
